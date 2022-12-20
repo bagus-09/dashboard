ecf11ab55aa257b2b6d5830fb48a8d68c8a97cf4
bagus-09 <117084642+bagus-09@users.noreply.github.com>
2022-12-20 16:17:23 +0000

    Add files via upload

---
+++ js/filter.js
@@ -0,0 +1,22 @@
+/**
+* Initiate Filter
+*/
+
+/*..................Card 1...........................................*/
+
+var cd1_filterValue = document.getElementById('cd1_filter-value');
+
+// $('#cd1_filter-item1').on('click', function(){
+//     alert("aaa");
+//     cd1_filterValue.innerHTML = this.innerHTML;
+// });
+
+// $('#cd1_filter-item2').on('click', function(){
+//     cd1_filterValue.innerHTML = this.innerHTML;
+// });
+
+// $('#cd1_filter-item3').on('click', function(){
+//     cd1_filterValue.innerHTML = this.innerHTML;
+// });
+/*.........................Card 1 END...............................*/
+
--- js/hidden-form.js
+++ js/hidden-form.js
@@ -9,16 +9,23 @@
 
 $('#offerType').change( function () {
 
-    const typeValue = document.getElementById('offerType').value;
+    const typeValue = this.value;
     const listItems = $('.list');
     const functionItems = $('.function');
+    const regularItems = $('.regular');
 
     if(typeValue === 'List'){
         listItems.show();
         functionItems.hide();
-    }else{
-        listItems.hide();
+        regularItems.hide();
+    }else if(typeValue === 'Function'){
         functionItems.show();
+        listItems.hide();
+        regularItems.hide();
+    }else{
+        regularItems.show();
+        listItems.hide();
+        functionItems.hide();
     }
 
 });
@@ -106,4 +113,294 @@ $('#offerRoles').change( function(){
         roleSelect.addClass('col-md-12');
     }
 
+});
+
+/**
+ * Hidden form element => Voucher Refill
+*/
+
+const full = $('.full');
+full.hide();
+
+$('#voucherType').change(function(){
+    const voucherValue = this.value;
+    const less = $('.less');
+    const full = $('.full');
+
+    if(voucherValue === 'Less'){
+        less.show();
+        full.hide();
+    }else{
+        less.hide();
+        full.show();
+    }
+
+});
+
+/**
+ * Hidden form element => Supervison date: XOR
+*/
+
+const supervision = $('.supervision');
+const supervisionRelative = $('.supervision-relative');
+
+$('#supervisionExpiryDate').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        supervisionRelative.show();
+    }else{
+        supervisionRelative.hide();
+    }
+});
+
+$('#supervisionExpiryDateRelative').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        supervision.show();
+    }else{
+        supervision.hide();
+    }
+});
+
+/**
+ * Hidden form element => Service Fee date: XOR
+*/
+
+const service = $('.service');
+const serviceRelative = $('.service-relative');
+
+$('#serviceFeeExpiryDate').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        serviceRelative.show();
+    }else{
+        serviceRelative.hide();
+    }
+});
+
+$('#serviceFeeExpiryDateRelative').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        service.show();
+    }else{
+        service.hide();
+    }
+});
+
+/**
+ * Hidden form element => DedicatedAccountInformation
+*/
+
+const updateInformationHide = $('.updateInformationHide');
+const updateInformationAccordion = $('.updateInformation');
+var count3 = 0;
+
+$('#updateInformationBtn').on('click', function(){
+    count3 += 1;
+
+    if(count3 % 2 != 0){
+        updateInformationHide.hide();
+
+        updateInformationAccordion.removeClass('col-md-6');
+        updateInformationAccordion.addClass('col-md-12');
+        
+    }else{
+        updateInformationHide.show();
+
+        updateInformationAccordion.removeClass('col-md-12');
+        updateInformationAccordion.addClass('col-md-6');
+        
+    }
+
+});
+
+/**
+ * Hidden form element => DedicatedAccountUpdateInformation : XOR
+*/
+
+const adjustmentAmount = $('.adjustment-amount');
+const dedicatedAccount = $('.dedicated-account');
+const adjustmentDate = $('.adjustment-date');
+const expiryDate = $('.expiry-date');
+
+$('#adjustmentAmountRelative').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        dedicatedAccount.show();
+    }else{
+        dedicatedAccount.hide();
+    }
+});
+
+
+$('#dedicatedAccount').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        adjustmentAmount.show();
+    }else{
+        adjustmentAmount.hide();
+    }
+});
+
+$('#adjustmentDate').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        expiryDate.show();
+    }else{
+        expiryDate.hide();
+    }
+});
+
+$('#offerExpiryDate').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        adjustmentDate.show();
+    }else{
+        adjustmentDate.hide();
+    }
+});
+
+/**
+ * Hidden form element => getAccountDetails, getAccumulator, getBalanceAndDate
+*/
+
+const accountDetails = $('.account-details');
+const balanceAccount = $('.balance-account');
+const accumulators = $('.accumulators');
+var count4 = 0, count5 = 0, count6 = 0;
+
+$('#detailsBtn').on('click', function(){
+    count4 += 1;
+
+    if(count4 % 2 != 0){
+        accountDetails.show();
+        balanceAccount.hide();
+        accumulators.hide();
+
+        accountDetails.removeClass('col-md-4');
+        accountDetails.addClass('col-md-12');
+        
+    }else{
+        accountDetails.show();
+        balanceAccount.show();
+        accumulators.show();
+
+        accountDetails.removeClass('col-md-12');
+        accountDetails.addClass('col-md-4');
+
+        balanceAccount.removeClass('col-md-12');
+        balanceAccount.addClass('col-md-4');
+
+        accumulators.removeClass('col-md-12');
+        accumulators.addClass('col-md-4');
+        
+    }
+});
+
+$('#balanceBtn').on('click', function(){
+    count5 += 1;
+
+    if(count5 % 2 != 0){
+        balanceAccount.show();
+        accountDetails.hide();
+        accumulators.hide();
+
+        balanceAccount.removeClass('col-md-4');
+        balanceAccount.addClass('col-md-12');
+        
+    }else{
+        accountDetails.show();
+        balanceAccount.show();
+        accumulators.show();
+
+        accountDetails.removeClass('col-md-12');
+        accountDetails.addClass('col-md-4');
+
+        balanceAccount.removeClass('col-md-12');
+        balanceAccount.addClass('col-md-4');
+
+        accumulators.removeClass('col-md-12');
+        accumulators.addClass('col-md-4');
+        
+    }
+});
+
+$('#accumulatorsBtn').on('click', function(){
+    count6 += 1;
+    
+    if(count6 % 2 != 0){
+        accumulators.show();
+        balanceAccount.hide();
+        accountDetails.hide();
+
+        accumulators.removeClass('col-md-4');
+        accumulators.addClass('col-md-12');
+        
+    }else{
+        accountDetails.show();
+        balanceAccount.show();
+        accumulators.show();
+
+        accountDetails.removeClass('col-md-12');
+        accountDetails.addClass('col-md-4');
+
+        balanceAccount.removeClass('col-md-12');
+        balanceAccount.addClass('col-md-4');
+
+        accumulators.removeClass('col-md-12');
+        accumulators.addClass('col-md-4');
+        
+    }
+});
+
+/**
+ * Hidden form element => getOffers
+ */ 
+
+const offer_selection = $('.offer-selection');
+const request_inactive = $('.request-inactive');
+const product_selection = $('.product-selection');
+
+$('#offerSelection').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        request_inactive.show();
+        product_selection.show();
+    }else{
+        request_inactive.hide();
+        product_selection.hide();
+    }
+});
+
+$('#requestInactiveOffers').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        offer_selection.show();
+        product_selection.show();
+    }else{
+        offer_selection.hide();
+        product_selection.hide();
+    }
+});
+
+$('#productSelection').on('input', function(){
+    const value = this.value;
+
+    if(value ===''){
+        offer_selection.show();
+        request_inactive.show();
+    }else{
+        offer_selection.hide();
+        request_inactive.hide();
+    }
 });
